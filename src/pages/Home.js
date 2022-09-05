843d10b16182ad567f33efb1371d8e148a25360b
Juno <juno1803@gmail.com>
2022-09-05 12:14:15 +0200

    add cart item + counter

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,18 @@
 import styled from "styled-components";
 import ShoppingItem from "../components/ShoppingItem";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
-export default function Home({ items, onAddToCart }) {
+export default function Home({ items, onAddToCart, counter }) {
   return (
     <>
       <h2>Search Items</h2>
+      <Link to="/shopping-cart">
+        <Div>
+          <img alt="" src="https://cdn4.iconfinder.com/data/icons/app-custom-ui-1/48/Basket_2-2-256.png" />
+          <Counter>{counter}</Counter>
+        </Div>
+      </Link>
       <CardContainer>
         {items.map((item) => {
           return (
@@ -17,7 +25,21 @@ export default function Home({ items, onAddToCart }) {
 }
 
 const CardContainer = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 10px;
+      display: flex;
+      flex-wrap: wrap;
+      gap: 10px;
+      `;
+
+const Counter = styled.p`
+// background-image: url("https://cdn4.iconfinder.com/data/icons/app-custom-ui-1/48/Basket_2-2-256.png");
+// background-repeat: no-repeat;
+// height: 150px;
+// background-size: contain;
+position: absolute;
+top: 75px;
+left: 140px;
+`;
+
+const Div = styled.div`
+position: relative;
 `;
